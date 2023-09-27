f97d765fa9dece1c77e2454339c075b080dd2ff9
Om Srivastava <79087911+omsrivastava512@users.noreply.github.com>
2023-09-27 10:15:12 +0530

    Added notes on format specifier

--- 1. First.c
+++ 1. First.c
@@ -1,6 +1,7 @@
 /* GUESS THE OUTPUT THEN COMPILE AND RUN THE CODE TO FIND OUT THE O/P
     */
-#include <stdio.h>
+#include <stdio.h>       // standard input-output headerfile: includes printf and scanf
+#include <limits.h>      // contains info about the size and range of various int data types
  
 
 int main()
@@ -22,8 +23,16 @@ int main()
     printf("\nThis is a new line \n");
     //here, \n is called an escape sequence and represents the newline character
 
+   //Other format specifiers
+   /*Integers*/
+   printf("\n%d is a signed integer",-5);           //-2,147,483,648 to 2,147,483,647 
+   printf("\n%u is a unsigned integer",5);         // 0 to 4,294,967,295
+   printf("\n%o is an octal integer",64);         //Convert to octal number system: 0 to 7, 10 to 17,...70 to 77, 100 to 777
+   printf("\n%x is a hexadecimal integer",256);   //Convert to hexadecimal number system: 0 to 9, a to f, 10 to 19, 1a to 1f,...100 to fff
+   
+
 
 
     
     return 0;
-}
+} 
